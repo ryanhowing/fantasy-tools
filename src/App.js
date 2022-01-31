9dfc84dc51f2c43919d150bb2cde00763695b8f5
ryanhowing <ryanhowing@gmail.com>
2022-01-31 22:26:00 +0800

    kk

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/Home'
 import Fixture from './components/Fixture'
 import Stats from './components/Stats'
+import Nav from './components/Nav'
 function App() {
   return (
     <BrowserRouter>
+    <Nav />
     <Routes>
-      <Route exact path="/" element={<Home/>}></Route>
-      <Route></Route>
+      <Route exact path="/" element={<Home />}></Route>
+      <Route path="fixture" element={<Fixture />}></Route>
+      <Route path="stats" element={<Stats />}></Route>
     </Routes>
     </BrowserRouter>
   );
